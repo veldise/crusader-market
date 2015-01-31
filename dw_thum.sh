cb533b88bf9dfab501e41568f324f51c37304c77
최영철 <veldise@gmail.com>
2015-01-31 11:15:53 +0000

    img 소스를 web에서 dir로 변경

---
+++ dw_thum.sh
@@ -0,0 +1,107 @@
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/6.gif > public/img/hero/thum/6.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/9.gif > public/img/hero/thum/9.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/12.gif > public/img/hero/thum/12.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/15.gif > public/img/hero/thum/15.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/18.gif > public/img/hero/thum/18.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/21.gif > public/img/hero/thum/21.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/243.gif > public/img/hero/thum/243.gif
+
+
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/46.gif > public/img/hero/thum/46.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/49.gif > public/img/hero/thum/49.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/52.gif > public/img/hero/thum/52.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/55.gif > public/img/hero/thum/55.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/58.gif > public/img/hero/thum/58.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/61.gif > public/img/hero/thum/61.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/247.gif > public/img/hero/thum/247.gif
+
+
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/86.gif > public/img/hero/thum/86.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/89.gif > public/img/hero/thum/89.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/92.gif > public/img/hero/thum/92.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/95.gif > public/img/hero/thum/95.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/98.gif > public/img/hero/thum/98.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/101.gif > public/img/hero/thum/101.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/251.gif > public/img/hero/thum/251.gif
+
+
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/126.gif > public/img/hero/thum/126.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/129.gif > public/img/hero/thum/129.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/132.gif > public/img/hero/thum/132.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/135.gif > public/img/hero/thum/135.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/138.gif > public/img/hero/thum/138.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/141.gif > public/img/hero/thum/141.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/255.gif > public/img/hero/thum/255.gif
+
+
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/166.gif > public/img/hero/thum/166.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/169.gif > public/img/hero/thum/169.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/172.gif > public/img/hero/thum/172.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/175.gif > public/img/hero/thum/175.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/178.gif > public/img/hero/thum/178.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/181.gif > public/img/hero/thum/181.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/259.gif > public/img/hero/thum/259.gif
+
+
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/206.gif > public/img/hero/thum/206.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/209.gif > public/img/hero/thum/209.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/212.gif > public/img/hero/thum/212.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/215.gif > public/img/hero/thum/215.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/218.gif > public/img/hero/thum/218.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/221.gif > public/img/hero/thum/221.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/hero/thum/263.gif > public/img/hero/thum/263.gif
+
+
+
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/3.gif > public/img/hero/skill/3.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/6.gif > public/img/hero/skill/6.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/9.gif > public/img/hero/skill/9.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/12.gif > public/img/hero/skill/12.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/15.gif > public/img/hero/skill/15.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/18.gif > public/img/hero/skill/18.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/111.gif > public/img/hero/skill/111.gif
+
+
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/21.gif > public/img/hero/skill/21.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/24.gif > public/img/hero/skill/24.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/27.gif > public/img/hero/skill/27.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/30.gif > public/img/hero/skill/30.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/33.gif > public/img/hero/skill/33.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/36.gif > public/img/hero/skill/36.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/115.gif > public/img/hero/skill/115.gif
+
+
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/39.gif > public/img/hero/skill/39.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/42.gif > public/img/hero/skill/42.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/45.gif > public/img/hero/skill/45.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/48.gif > public/img/hero/skill/48.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/51.gif > public/img/hero/skill/51.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/54.gif > public/img/hero/skill/54.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/119.gif > public/img/hero/skill/119.gif
+
+
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/57.gif > public/img/hero/skill/57.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/60.gif > public/img/hero/skill/60.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/63.gif > public/img/hero/skill/63.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/66.gif > public/img/hero/skill/66.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/69.gif > public/img/hero/skill/69.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/72.gif > public/img/hero/skill/72.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/123.gif > public/img/hero/skill/123.gif
+
+
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/75.gif > public/img/hero/skill/75.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/77.gif > public/img/hero/skill/77.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/81.gif > public/img/hero/skill/81.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/84.gif > public/img/hero/skill/84.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/87.gif > public/img/hero/skill/87.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/90.gif > public/img/hero/skill/90.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/127.gif > public/img/hero/skill/127.gif
+
+
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/93.gif > public/img/hero/skill/93.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/96.gif > public/img/hero/skill/96.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/99.gif > public/img/hero/skill/99.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/102.gif > public/img/hero/skill/102.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/105.gif > public/img/hero/skill/105.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/108.gif > public/img/hero/skill/108.gif
+curl http://appdata.hungryapp.co.kr/images/dbimg/crusadersquest/skill/131.gif > public/img/hero/skill/131.gif

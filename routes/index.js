73c5c53b3806d5686590d8ce13d006c5fcaacd3e
최영철 <veldise@gmail.com>
2015-02-01 12:39:26 +0000

    version 표기를 v0.1.1로 변경

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,5 @@ exports.index = function(req, res){
     var remoteIP = req.ip || req.connection.remoteAddress;
     console.log('access index.jade:', remoteIP, req.headers['user-agent']);
 
-    res.render('index', { title: 'Express' });
+    res.render('index', { version: 'v0.1.1' });
 };

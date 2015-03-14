fc778601bceeef66f9f1bdcb5d3121ee39ae36b5
최영철 <veldise@gmail.com>
2015-03-14 06:54:17 +0000

    tsv 2 json 스크립트 작성, 데이터 추가 업데이트

---
+++ scripts/tsv2json.js
@@ -0,0 +1,93 @@
+#!/usr/local/bin/node
+
+'use strict';
+/**
+*   Module dependencies
+*/
+var fs = require('fs'),
+    path = require('path'),
+    _ = require('lodash');
+/**
+*   process arguments
+*/
+if (process.argv.length < 2) {
+    console.error([
+        'usage:',
+        path.basename(__filename, '.js'),
+        '[-r]',
+        '[-f]',
+        '<filepath>'
+    ].join(' '));
+    process.exit();
+}
+
+var tsvPath = process.argv[2],
+    jsonPath = process.argv[3] || tsvPath.replace(/\.tsv$/, '.json');
+
+console.log(tsvPath, ' to ', jsonPath);
+
+fs.readFile(tsvPath, 'utf8', function (err, data) {
+    if (err) {
+        throw err;
+    }
+    var json = tsv2json(removeBOM(data));
+
+    fs.writeFile(jsonPath, JSON.stringify(json, null, 2), 'utf8', function (err) {
+        if (err) {
+            throw err;
+        }
+        console.log('converted.');
+    });
+});
+
+function tsv2json (tsv) {
+    var json;
+
+    var lines = tsv.split(/\r?\n/);
+    lines = _.compact(_.map(lines, function (line) {
+        if (line.trim()) {
+            line = line.trim().split(/\t/);
+            for (var i=line.length-1; i>=0; i--) {
+                line[i] = convTypes(line[i]);
+            }
+            return line;
+        }
+        else {
+            return null;
+        }
+    }));
+    var header = lines.shift();
+
+    return _.map(lines, function (line) {
+        return _.zipObject(header, line);
+    });
+}
+
+function removeBOM (str) {
+    var buf = new Buffer(str);
+    if ((buf[0] === 0xEF) && (buf[1] === 0xBB) && (buf[2] === 0xBF)) {
+        return buf.slice(3).toString();
+    }
+    return buf.toString();
+}
+
+function convTypes (str) {
+    if (!str) {
+        return null;
+    }
+
+    // number
+    if (!_.isNaN(+str)) {
+        return +str;
+    }
+    // boolean
+    else if (str.toLowerCase() === 'true') {
+        return true;
+    }
+    else if (str.toLowerCase() === 'false') {
+        return false;
+    }
+    else {
+        return str.replace(/^\"/, '').replace(/\"$/, '');
+    }
+}

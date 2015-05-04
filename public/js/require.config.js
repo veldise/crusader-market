00f2ce54ffb40b515c007e5135953a0862e653f0
최영철 <veldise@gmail.com>
2015-05-04 13:43:43 +0000

    require.config 파일 분리

---
+++ public/js/require.config.js
@@ -0,0 +1,30 @@
+/**
+*
+*/
+require.config({
+    paths: {
+        'jquery': '/lib/jquery/jquery-1.11.2.min',
+        'lodash': '/lib/lodash.min',
+
+        'angular': '/lib/angular/angular.min',
+        'angular-route': '/lib/angular/angular-route.min',
+
+        'bootstrap': '/lib/bootstrap-3.3.4/js/bootstrap.min',
+        'ui-bootstrap': '/lib/ui-bootstrap-tpls-0.13.0.min',
+
+        'smart-table': '/lib/smart-table.min'
+    },
+
+    shim: {
+        'jquery': { exports: '$' },
+        'lodash': { exports: '_' },
+
+        'angular': { exports: 'angular', deps: ['jquery'] },
+        'angular-route': ['angular'],
+
+        'bootstrap': ['jquery'],
+        'ui-bootstrap': ['angular'],
+
+        'smart-table': ['angular']
+    }
+});
